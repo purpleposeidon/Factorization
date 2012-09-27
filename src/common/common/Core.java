9e43099541e023c8b1163ebad3cfdb849530bfb3
neptunepink <purpleposeidon@gmail.com>
2012-09-27 12:54:46 -0700

    this is needed for the name command

--- src/common/common/Core.java
+++ src/common/common/Core.java
@@ -19,11 +19,15 @@ import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.PostInit;
 import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.Mod.ServerStarted;
+import cpw.mods.fml.common.Mod.ServerStarting;
 import cpw.mods.fml.common.Side;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartedEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.IGuiHandler;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.network.NetworkRegistry;
@@ -217,6 +221,12 @@ public class Core {
     public void modsLoaded(FMLPostInitializationEvent event) {
         TileEntityWrathFire.setupBurning();
         registry.addDictOres();
+    }
+    
+    
+    @ServerStarting
+    public void registerServerCommands(FMLServerStartingEvent event) {
+        event.registerServerCommand(new NameClayCommand());
     }
 
     ItemStack getExternalItem(String className, String classField, String description) {
