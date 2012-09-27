9a08a199c5231a809aeb6e02b6b76dedd5b6cdd5
neptunepink <purpleposeidon@gmail.com>
2012-09-27 12:52:20 -0700

    Match renderer with dark iron free recipe

--- src/client/render/BlockRenderGrinder.java
+++ src/client/render/BlockRenderGrinder.java
@@ -22,19 +22,20 @@ public class BlockRenderGrinder extends FactorizationBlockRender {
         float p2 = 2*p, p3 = 3*p;
         int dark_iron = 3+16*2, metal = 14, lead = 2+16*2;;
         //bottom plate
-        renderPart(rb, dark_iron, 2*p, 0, 2*p, 1-2*p, 2*p, 1-2*p);
+        //renderPart(rb, metal, 2*p, 0, 2*p, 1-2*p, 2*p, 1-2*p);
+        renderPart(rb, metal, 0, 0, 0, 1, 2*p, 1);
         //top cap
         renderPart(rb, metal, 0, 1-p3, 0, 1, 1-p, 1);
         //side edges
-        renderPart(rb, metal, 0, 0, 0, p2, 1-p3, p2);
-        renderPart(rb, metal, 1-p2, 0, 1-p2, 1, 1-p3, 1);
-        renderPart(rb, metal, 0, 0, 1-p2, p2, 1-p3, 1);
-        renderPart(rb, metal, 1-p2, 0, 0, 1, 1-p3, p2);
+        renderPart(rb, metal, 0, p2, 0, p2, 1-p3, p2);
+        renderPart(rb, metal, 1-p2, p2, 1-p2, 1, 1-p3, 1);
+        renderPart(rb, metal, 0, p2, 1-p2, p2, 1-p3, 1);
+        renderPart(rb, metal, 1-p2, p2, 0, 1, 1-p3, p2);
         //bottom edges
-        renderPart(rb, lead, 1-p2, 0, p2, p2, p*4, 0);
-        renderPart(rb, lead, 1-p2, 0, 1, p2, p*4, 1-p2);
-        renderPart(rb, lead, 0, 0, p2, p2, p*4, 1-p2);
-        renderPart(rb, lead, 1-p2, 0, p2, 1, p*4, 1-p2);
+        renderPart(rb, lead, 1-p2, p2, p2, p2, p*4, 0);
+        renderPart(rb, lead, 1-p2, p2, 1, p2, p*4, 1-p2);
+        renderPart(rb, lead, 0, p2, p2, p2, p*4, 1-p2);
+        renderPart(rb, lead, 1-p2, p2, p2, 1, p*4, 1-p2);
         
         if (!world_mode) {
             GL11.glPushMatrix();
