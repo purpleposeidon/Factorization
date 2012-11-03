47bcfb39117534025c4bff3e201125ed88c5c0b6
neptunepink <purpleposeidon@gmail.com>
2012-11-02 22:36:01 -0700

    I guess we're done. Time to start World Entities!

--- src/common/common/Core.java
+++ src/common/common/Core.java
@@ -47,7 +47,7 @@ import factorization.client.gui.FactorizationNotify;
         channels = { NetworkFactorization.factorizeTEChannel, NetworkFactorization.factorizeMsgChannel, NetworkFactorization.factorizeCmdChannel, NetworkFactorization.factorizeNtfyChannel })
 public class Core {
     //The comment below is a marker used by the build script.
-    public static final String version = "0.6.10"; //@VERSION@
+    public static final String version = "0.6.11"; //@VERSION@
     public Core() {
         registry = new Registry();
         exoCore = new ExoCore();
