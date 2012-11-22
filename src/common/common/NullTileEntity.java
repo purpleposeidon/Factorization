f8d436e889b81dfce5d011c652783e910cb88364
neptunepink <purpleposeidon@gmail.com>
2012-11-22 10:06:15 -0800

    Hmm, forgot this fella

---
+++ src/common/common/NullTileEntity.java
@@ -0,0 +1,26 @@
+package factorization.common;
+
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Side;
+import net.minecraft.src.TileEntity;
+import factorization.api.Coord;
+import factorization.common.NetworkFactorization.MessageType;
+
+public class NullTileEntity extends TileEntity {
+    int description_request_delay = 5;
+    int fails = 1;
+    
+    @Override
+    public void updateEntity() {
+        if (FMLCommonHandler.instance().getEffectiveSide() != Side.CLIENT) {
+            new Coord(this).removeTE();
+        }
+        if (description_request_delay-- == 0) {
+            Coord here = new Coord(this);
+            Core.logFine("%s: asking for description packet", here);
+            description_request_delay = 20*fails;
+            Core.network.broadcastMessage(null, here, MessageType.DescriptionRequest);
+            fails++;
+        }
+    }
+}
