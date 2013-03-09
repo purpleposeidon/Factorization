c8e13543def09a8ea0d4a9c6fc980f8a6ea83006
neptunepink <purpleposeidon@gmail.com>
2013-03-09 00:06:56 -0800

    Heater takes redstone signal now

--- src/common/common/TileEntityHeater.java
+++ src/common/common/TileEntityHeater.java
@@ -97,6 +97,10 @@ public class TileEntityHeater extends TileEntityCommon implements IChargeConduct
         }
         updateClient();
         Coord here = getCoord();
+        if (here.isPowered()) {
+            charge.update();
+            return;
+        }
         long now = worldObj.getWorldTime() + here.seed();
         int rate = 4;
         if (now % rate == 0) {
