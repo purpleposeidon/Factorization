df14a7a8a0050af5967044ca835717bcf8df115e
neptunepink <purpleposeidon@gmail.com>
2013-04-10 22:08:08 -0700

    Added a hint system for items; starting with sludge

--- src/common/common/ItemCraftingComponent.java
+++ src/common/common/ItemCraftingComponent.java
@@ -8,15 +8,24 @@ import net.minecraft.item.ItemStack;
 import factorization.common.Core.TabType;
 
 public class ItemCraftingComponent extends Item {
+    String hint_text = null;
 
     public ItemCraftingComponent(int id, String name) {
         super(id);
         setUnlocalizedName("factorization:" + name.replace('.', '/'));
         Core.tab(this, TabType.MATERIALS);
     }
-
+    
+    public ItemCraftingComponent setHint(String hint) {
+        this.hint_text = hint;
+        return this;
+    }
+    
     @Override
     public void addInformation(ItemStack is, EntityPlayer player, List infoList, boolean verbose) {
+        if (hint_text != null) {
+            infoList.add(hint_text);
+        }
         Core.brand(infoList);
     }
 }
