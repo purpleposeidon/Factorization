6dcbc148e1262224c6a1955207899d7369f351d6
neptunepink <purpleposeidon@gmail.com>
2013-05-05 00:30:04 -0700

    Oh, you know what, time for a MAJOR version bunmp. BUNMP! 0.8!
    Except servo motors are supposed to be in 0.9. :/
    This one's supposed to be rockets! Oh well.
    They can still be in 0.8.

--- src/common/common/Core.java
+++ src/common/common/Core.java
@@ -50,7 +50,7 @@ public class Core {
     public static final String modId = "factorization";
     public static final String name = "Factorization";
     //The comment below is a marker used by the build script.
-    public static final String version = "0.7.38"; //@VERSION@
+    public static final String version = "0.8.00"; //@VERSION@
     public Core() {
         registry = new Registry();
         exoCore = new ExoCore();
