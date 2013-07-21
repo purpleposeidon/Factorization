4df1cff79b62228d5b91e35a6b047bed445d3134
neptunepink <purpleposeidon@gmail.com>
2013-07-21 03:31:45 -0700

    Aaaand tabstuff

--- src/common/common/ItemCraftingComponent.java
+++ src/common/common/ItemCraftingComponent.java
@@ -5,6 +5,6 @@ import factorization.common.Core.TabType;
 
 public class ItemCraftingComponent extends ItemFactorization { //NORELEASE: Move stuff that isn't expensive into a single ID
     public ItemCraftingComponent(int id, String name) {
-        super(id, name, TabType.MISC);
+        super(id, name, TabType.MATERIALS);
     }
 }
