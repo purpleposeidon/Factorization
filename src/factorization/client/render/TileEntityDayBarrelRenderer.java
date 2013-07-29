3bc706ddc22f4f7ad5e77e5b392c48d2e9a06a37
neptunepink <purpleposeidon@gmail.com>
2013-07-29 08:52:39 -0700

    Shift the render down a tad

--- src/factorization/client/render/TileEntityDayBarrelRenderer.java
+++ src/factorization/client/render/TileEntityDayBarrelRenderer.java
@@ -44,7 +44,7 @@ public class TileEntityDayBarrelRenderer extends TileEntitySpecialRenderer {
         Quaternion quat = Quaternion.fromOrientation(bo.getSwapped());
         quat.glRotate();
         GL11.glRotatef(90, 0, 1, 0);
-        GL11.glTranslated(0.25, 0.25, -1.0/128.0);
+        GL11.glTranslated(0.25, 0.25 - 1.0/16.0, -1.0/128.0);
         
         renderItemCount(barrel);
         handleRenderItem(is);
