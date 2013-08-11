0317530384a65a96c638131db4c9f519b9f5f5aa
neptunepink <purpleposeidon@gmail.com>
2013-08-10 18:32:48 -0700

    Hopefully fixed parasieve recursion.
    I'm so glad I'm programming in a functional language. This would be much
    more annoying to do in a less expressive one.

--- src/factorization/common/TileEntityParaSieve.java
+++ src/factorization/common/TileEntityParaSieve.java
@@ -53,57 +53,55 @@ public class TileEntityParaSieve extends TileEntityFactorization implements ISid
     }
     
     boolean itemPassesFilter(ItemStack stranger) {
+        boolean empty = true;
         for (int i = 0; i < filters.length/2; i++) {
             ItemStack a = filters[i*2], b = filters[i*2 + 1];
-            if (a == b && b == null) {
+            if (a == null && b == null) {
                 continue;
             }
+            empty = false;
             if (FactorizationUtil.itemInRange(a, b, stranger)) {
                 return true;
             }
         }
-        return false;
+        return empty;
     }
     
-    private boolean is_recursing = false;
-    private static ThreadLocal<Integer> recursion_count = new ThreadLocal<Integer>() { @Override
-    protected Integer initialValue() {return 0;};};
+    private byte self_recursion = 0;
+    private static ThreadLocal<Integer> stack_recursion = new ThreadLocal<Integer>() {
+        @Override protected Integer initialValue() {return 0;};
+    };
     
-    protected boolean beginRecursion() {
-        recursion_count.set(recursion_count.get() + 1);
-        if (is_recursing || recursion_count.get() > 6) {
+    protected boolean _beginRecursion() {
+        int sr = stack_recursion.get() + 1;
+        stack_recursion.set(sr);
+        self_recursion++;
+        if (self_recursion > 1 || sr > 6) {
             return true;
         }
-        is_recursing = true;
         return false;
     }
     
     protected void endRecursion() {
-        is_recursing = false;
-        recursion_count.set(Math.min(0, recursion_count.get() - 1));
+        self_recursion = (byte) Math.max(0, self_recursion - 1);
+        int sr = stack_recursion.get();
+        stack_recursion.set(Math.max(0, sr - 1));
     }
     
-    IInventory getTarget() {
-        try {
-            if (beginRecursion() || putting_nbt || worldObj == null || worldObj.isRemote) {
-                return null;
-            }
-            ForgeDirection facing = getFacing();
-            if (facing == ForgeDirection.UNKNOWN) {
-                return null;
-            }
-            //return getCoord().add(facing).getTE(IInventory.class);
-            TileEntity te = worldObj.getBlockTileEntity(xCoord + facing.offsetX, yCoord + facing.offsetY, zCoord + facing.offsetZ);
-            if (te instanceof IInventory) {
-                if (te instanceof TileEntityParaSieve) {
-                    return null;
-                }
-                return FactorizationUtil.openDoubleChest((IInventory) te, true);
-            }
+    IInventory getRecursiveTarget() {
+        if (_beginRecursion() || putting_nbt || worldObj == null || worldObj.isRemote) {
             return null;
-        } finally {
-            endRecursion();
         }
+        ForgeDirection facing = getFacing();
+        if (facing == ForgeDirection.UNKNOWN) {
+            return null;
+        }
+        //return getCoord().add(facing).getTE(IInventory.class);
+        TileEntity te = worldObj.getBlockTileEntity(xCoord + facing.offsetX, yCoord + facing.offsetY, zCoord + facing.offsetZ);
+        if (te instanceof IInventory) {
+            return FactorizationUtil.openDoubleChest((IInventory) te, true);
+        }
+        return null;
     }
     
     @Override
@@ -111,11 +109,15 @@ public class TileEntityParaSieve extends TileEntityFactorization implements ISid
         if (putting_nbt) {
             return filters.length;
         }
-        IInventory target = getTarget();
-        if (target == null) {
-            return filters.length;
+        try {
+            IInventory target = getRecursiveTarget();
+            if (target == null) {
+                return filters.length;
+            }
+            return filters.length + target.getSizeInventory();
+        } finally {
+            endRecursion();
         }
-        return filters.length + target.getSizeInventory();
     }
 
     @Override
@@ -123,12 +125,15 @@ public class TileEntityParaSieve extends TileEntityFactorization implements ISid
         if (i < filters.length) {
             return filters[i];
         }
-        IInventory target = getTarget();
-        if (target == null) {
-            return null;
+        try {
+            IInventory target = getRecursiveTarget();
+            if (target == null) {
+                return null;
+            }
+            return target.getStackInSlot(i - filters.length);
+        } finally {
+            endRecursion();
         }
-        return target.getStackInSlot(i - filters.length);
-        
     }
 
     @Override
@@ -137,11 +142,15 @@ public class TileEntityParaSieve extends TileEntityFactorization implements ISid
             filters[i] = itemstack;
             return;
         }
-        IInventory target = getTarget();
-        if (target == null) {
-            return;
+        try {
+            IInventory target = getRecursiveTarget();
+            if (target == null) {
+                return;
+            }
+            target.setInventorySlotContents(i - filters.length, itemstack);
+        } finally {
+            endRecursion();
         }
-        target.setInventorySlotContents(i - filters.length, itemstack);
     }
 
     @Override
@@ -154,33 +163,41 @@ public class TileEntityParaSieve extends TileEntityFactorization implements ISid
         if (i < filters.length) {
             return true;
         }
-        IInventory target = getTarget();
-        if (target == null) {
-            return false;
+        try {
+            IInventory target = getRecursiveTarget();
+            if (target == null) {
+                return false;
+            }
+            return target.isItemValidForSlot(i - filters.length, itemstack) && itemPassesFilter(itemstack);
+        } finally {
+            endRecursion();
         }
-        return target.isItemValidForSlot(i - filters.length, itemstack) && itemPassesFilter(itemstack);
     }
 
     @Override
     public int[] getAccessibleSlotsFromSide(int side) {
-        IInventory target = getTarget();
-        if (target == null) {
-            return new int[0];
-        }
-        side = facing_direction;
-        if (target instanceof ISidedInventory) {
-            int[] ret = Arrays.clone(((ISidedInventory)target).getAccessibleSlotsFromSide(side));
-            for (int i = 0; i < ret.length; i++) {
-                ret[i] += filters.length;
+        try {
+            IInventory target = getRecursiveTarget();
+            if (target == null) {
+                return new int[0];
             }
-            return ret;
-        } else {
-            int len = target.getSizeInventory();
-            int[] ret = new int[len];
-            for (int i = 0; i < len; i++) {
-                ret[i] = i + filters.length;
+            side = facing_direction;
+            if (target instanceof ISidedInventory) {
+                int[] ret = Arrays.clone(((ISidedInventory)target).getAccessibleSlotsFromSide(side));
+                for (int i = 0; i < ret.length; i++) {
+                    ret[i] += filters.length;
+                }
+                return ret;
+            } else {
+                int len = target.getSizeInventory();
+                int[] ret = new int[len];
+                for (int i = 0; i < len; i++) {
+                    ret[i] = i + filters.length;
+                }
+                return ret;
             }
-            return ret;
+        } finally {
+            endRecursion();
         }
     }
     
@@ -189,14 +206,18 @@ public class TileEntityParaSieve extends TileEntityFactorization implements ISid
         if (slot < filters.length) {
             return true;
         }
-        IInventory target = getTarget();
-        if (target == null) {
-            return true;
+        try {
+            IInventory target = getRecursiveTarget();
+            if (target == null) {
+                return true;
+            }
+            if (target instanceof ISidedInventory) {
+                return ((ISidedInventory) target).canInsertItem(slot - filters.length, itemstack, side) && itemPassesFilter(itemstack);
+            }
+            return itemPassesFilter(itemstack);
+        } finally {
+            endRecursion();
         }
-        if (target instanceof ISidedInventory) {
-            return ((ISidedInventory) target).canInsertItem(slot - filters.length, itemstack, side) && itemPassesFilter(itemstack);
-        }
-        return itemPassesFilter(itemstack);
     }
     
     @Override
@@ -204,14 +225,18 @@ public class TileEntityParaSieve extends TileEntityFactorization implements ISid
         if (slot < filters.length) {
             return true;
         }
-        IInventory target = getTarget();
-        if (target == null) {
-            return true;
+        try {
+            IInventory target = getRecursiveTarget();
+            if (target == null) {
+                return true;
+            }
+            if (target instanceof ISidedInventory) {
+                return ((ISidedInventory) target).canExtractItem(slot - filters.length, itemstack, facing_direction) && itemPassesFilter(itemstack);
+            }
+            return itemPassesFilter(itemstack);
+        } finally {
+            endRecursion();
         }
-        if (target instanceof ISidedInventory) {
-            return ((ISidedInventory) target).canExtractItem(slot - filters.length, itemstack, facing_direction) && itemPassesFilter(itemstack);
-        }
-        return itemPassesFilter(itemstack);
     }
     
     @Override
@@ -238,7 +263,7 @@ public class TileEntityParaSieve extends TileEntityFactorization implements ISid
     @Override
     public int getComparatorValue(ForgeDirection side) {
         try {
-            if (beginRecursion()) {
+            if (_beginRecursion()) {
                 return 11;
             }
             return getCoord().add(getFacing()).getComparatorOverride(getFacing().getOpposite());
@@ -266,9 +291,14 @@ public class TileEntityParaSieve extends TileEntityFactorization implements ISid
     @Override
     public void onInventoryChanged() {
         super.onInventoryChanged();
-        IInventory inv = getTarget();
-        if (inv != null) {
+        try {
+            IInventory inv = getRecursiveTarget();
+            if (inv == null) {
+                return;
+            }
             inv.onInventoryChanged();
+        } finally {
+            endRecursion();
         }
     }
 }
