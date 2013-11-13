a14a98901bd3fe68b1012e5ca69a6b6b74969978
neptunepink <purpleposeidon@gmail.com>
2013-11-12 21:41:31 -0800

    version bunmph

--- src/factorization/common/Core.java
+++ src/factorization/common/Core.java
@@ -50,7 +50,7 @@ public class Core {
     public static final String modId = "factorization";
     public static final String name = "Factorization";
     //The comment below is a marker used by the build script.
-    public static final String version = "0.8.10"; //@VERSION@
+    public static final String version = "0.8.11"; //@VERSION@
     public Core() {
         instance = this;
         fzconfig = new FzConfig();
