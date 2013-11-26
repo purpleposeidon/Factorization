595e5f3e3185a23a91573d0f944e19583cd7b40b
neptunepink <purpleposeidon@gmail.com>
2013-11-26 02:28:08 -0800

    TC4 integration notes

--- src/factorization/common/compat/Compat_Thaumcraft.java
+++ src/factorization/common/compat/Compat_Thaumcraft.java
@@ -13,8 +13,9 @@ import thaumcraft.api.research.ScanResult;*/
 public class Compat_Thaumcraft {
     //Reminder: This needs to be mentioned in CompactManager
     public Compat_Thaumcraft() {
-        //NORELEASE: Register TC scan handler for barrels
-        //NORELEASE: Is azanor cool with this being released if TC4 isn't?
+        //TODO: SoundLogic'll give me item aspects?
+        //TODO: Make sure we give a way to disable this!
+        //TODO: Register TC scan handler for barrels; should give its contents rather than the barrel
         /*ThaumcraftApi.registerScanEventhandler(new IScanEventHandler() {
             
             @Override
