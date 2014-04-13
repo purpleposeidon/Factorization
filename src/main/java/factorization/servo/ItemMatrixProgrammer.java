b85bb965c365b86ffc45654266d18f82dd165616
neptunepink <purpleposeidon@gmail.com>
2014-04-13 03:41:44 -0700

    Add painting adjustmenting

--- src/main/java/factorization/servo/ItemMatrixProgrammer.java
+++ src/main/java/factorization/servo/ItemMatrixProgrammer.java
@@ -1,24 +1,35 @@
 package factorization.servo;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.item.EntityPainting;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.EntityInteractEvent;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import factorization.api.Coord;
 import factorization.shared.Core;
-import factorization.shared.ItemFactorization;
 import factorization.shared.Core.TabType;
+import factorization.shared.FzUtil;
+import factorization.shared.ItemFactorization;
 
 public class ItemMatrixProgrammer extends ItemFactorization {
     public ItemMatrixProgrammer() {
         super("tool.matrix_programmer", TabType.TOOLS);
         setMaxStackSize(1);
         setContainerItem(this);
+        Core.loadBus(this);
     }
     
     @Override
     public boolean doesContainerItemLeaveCraftingGrid(ItemStack par1ItemStack) {
         return false;
     }
+    
+    @EventHandler
     
     @Override
     public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
@@ -48,5 +59,40 @@ public class ItemMatrixProgrammer extends ItemFactorization {
     @Override
     public boolean isItemTool(ItemStack is) {
         return true;
+    }
+    
+    @SubscribeEvent
+    public void clickPainting(EntityInteractEvent event) {
+        ItemStack is = event.entityPlayer.getHeldItem();
+        if (is == null) return;
+        if (is.getItem() != this) return;
+        if (!(event.target instanceof EntityPainting)) {
+            return;
+        }
+        int d = event.entityPlayer.isSneaking() ? -1 : 1;
+        EntityPainting painting = (EntityPainting) event.target;
+        if (painting.worldObj.isRemote) {
+            return;
+        }
+        EntityPainting.EnumArt origArt = painting.art;
+        EntityPainting.EnumArt art = origArt;
+        int hangingDirection = painting.hangingDirection;
+        while (true) {
+            art = FzUtil.shiftEnum(art, EntityPainting.EnumArt.values(), d);
+            painting.setDirection(hangingDirection);
+            painting.art = art;
+            if (art == origArt) {
+                return;
+            }
+            if (!painting.onValidSurface()) {
+                continue;
+            }
+            NBTTagCompound save = new NBTTagCompound();
+            painting.writeToNBTOptional(save);
+            painting.setDead();
+            Entity newPainting = EntityList.createEntityFromNBT(save, event.target.worldObj);
+            newPainting.worldObj.spawnEntityInWorld(newPainting);
+            break;
+        }
     }
 }
