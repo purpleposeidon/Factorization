399a2f704ec74f15ad0edb5932be536b012af99b
neptunepink <purpleposeidon@gmail.com>
2014-07-28 16:58:58 -0700

    Make interactions behave more like vanilla

--- src/main/java/factorization/fzds/ClickHandler.java
+++ src/main/java/factorization/fzds/ClickHandler.java
@@ -3,8 +3,11 @@ package factorization.fzds;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityClientPlayerMP;
+import net.minecraft.client.multiplayer.PlayerControllerMP;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
@@ -18,6 +21,7 @@ import cpw.mods.fml.common.network.internal.FMLProxyPacket;
 import factorization.api.Coord;
 import factorization.fzds.HammerNet.HammerNetType;
 import factorization.fzds.api.DeltaCapability;
+import factorization.shared.FzUtil;
 
 public class ClickHandler {
     //Note that these events will be triggered client-side only, as this entity is only used client-side.
@@ -66,16 +70,28 @@ public class ClickHandler {
                         (float) (hit.hitVec.xCoord - hit.blockX),
                         (float) (hit.hitVec.yCoord - hit.blockY),
                         (float) (hit.hitVec.zCoord - hit.blockZ));
+                Coord at = new Coord(DeltaChunk.getClientShadowWorld(), hit);
+                Block block = at.getBlock();
+                EntityPlayer real_player = Minecraft.getMinecraft().thePlayer;
+                Hammer.proxy.setShadowWorld();
+                try {
+                    EntityPlayer shadow_player = Minecraft.getMinecraft().thePlayer;
+                    if (block.onBlockActivated(at.w, at.x, at.y, at.z, shadow_player, hit.sideHit, (float) hit.hitVec.xCoord, (float) hit.hitVec.yCoord, (float) hit.hitVec.zCoord)) {
+                        real_player.swingItem();
+                    }
+                } finally {
+                    Hammer.proxy.restoreRealWorld();
+                }
             } else {
                 if (!parent.can(DeltaCapability.BLOCK_MINE)) {
                     return;
                 }
-                //TODO XXX FIXME: Implement proper digging
                 toSend = HammerNet.makePacket(HammerNetType.leftClickBlock, parent.getEntityId(), hit,
                         (float) (hit.hitVec.xCoord - hit.blockX),
                         (float) (hit.hitVec.yCoord - hit.blockY),
                         (float) (hit.hitVec.zCoord - hit.blockZ));
                 current_attacking_target = hit;
+                // Digging code happens in a ticker
             }
             break;
         default: return;
@@ -102,50 +118,105 @@ public class ClickHandler {
                 || current_attacking_target.blockY != hit.blockY
                 || current_attacking_target.blockZ != hit.blockZ
                 || current_attacking_target.subHit != hit.subHit) {
-            // NORELEASE: Cursor moved off the block. Make it so that the click can re-fire
+            resetClick();
             resetProgress();
             return;
         }
-        EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+        tickClickBlock(hit);
+    }
+    
+    void sendDigPacket(byte packetType, MovingObjectPosition hit) {
+        FMLProxyPacket toSend = HammerNet.makePacket(packetType, Hammer.proxy.getHitIDC().getEntityId(), hit);
+        HammerNet.channel.sendToServer(toSend);
+    }
+    
+    ItemStack original_tool;
+    MovingObjectPosition original_block;
+    
+    void tickClickBlock(MovingObjectPosition hit) {
+        if (left_click_delay > 0) {
+            left_click_delay--;
+            return;
+        }
+        EntityClientPlayerMP player = Minecraft.getMinecraft().thePlayer;
+        if (player == null) return;
         World shadowWorld = DeltaChunk.getClientShadowWorld();
         Minecraft mc = Minecraft.getMinecraft();
-        if (player == null) return;
-        
         Coord at = new Coord(shadowWorld, hit);
         Block hitBlock = at.getBlock();
-        if (hitBlock == null || hitBlock.getMaterial() == Material.air) {
-            resetProgress();
-            return;
-        }
-        if (progress == 0) {
-            hitBlock.onBlockClicked(shadowWorld, hit.blockX, hit.blockY, hit.blockZ, player);
-        }
-        //float relativeHardness = hitBlock.getPlayerRelativeBlockHardness(player, shadowWorld, hit.blockX, hit.blockY, hit.blockZ);
+        PlayerControllerMP controller = mc.playerController;
         if (!(mc.currentScreen == null && mc.gameSettings.keyBindAttack.getIsKeyPressed() && mc.inGameHasFocus)) {
             resetProgress();
             return;
         }
-        player.swingItem();
-        int int_progress = (int) (progress/10F);
-        byte packetType = progress == 0 ? HammerNetType.digStart : HammerNetType.digProgress;
-        progress++;
-        if (int_progress >= 10) {
-            packetType = HammerNetType.digFinish;
-        }
-        FMLProxyPacket toSend = HammerNet.makePacket(packetType, Hammer.proxy.getHitIDC().getEntityId(), hit);
-        HammerNet.channel.sendToServer(toSend);
-        if (int_progress >= 10) {
-            resetProgress();
-            System.out.println("NORELEASE: Pop!");
-            // NORELEASE: Refire the click here as well!
+        if (controller.currentGameType.isAdventure() && !player.isCurrentToolAdventureModeExempt(hit.blockX, hit.blockY, hit.blockZ)) return;
+        if (controller.currentGameType.isCreative()) {
+            sendDigPacket(HammerNetType.digFinish, hit);
             Hammer.proxy.setShadowWorld();
             try {
-                Minecraft.getMinecraft().playerController.onPlayerDestroyBlock(hit.blockX, hit.blockY, hit.blockZ, hit.sideHit);
+                if (shadowWorld.extinguishFire(player, hit.blockX, hit.blockY, hit.blockZ, hit.sideHit)) return; // :|
+                controller.onPlayerDestroyBlock(hit.blockX, hit.blockY, hit.blockZ, hit.sideHit);
+            }
+            finally {
+                Hammer.proxy.restoreRealWorld();
+            }
+            return;
+        }
+        if (hitBlock == null || hitBlock.getMaterial() == Material.air) {
+            resetProgress();
+            return;
+        }
+        
+        if (progress == 0) {
+            hitBlock.onBlockClicked(shadowWorld, hit.blockX, hit.blockY, hit.blockZ, player);
+            original_block = hit;
+            original_tool = player.getHeldItem();
+        } else {
+            ItemStack held = player.getHeldItem();
+            if (original_tool != held) {
+                if (original_tool == null || held == null) {
+                    resetProgress();
+                    return;
+                }
+                if (original_tool.getItem() != held.getItem()) {
+                    // (Personally, I think the progress should be preserved if the player switches to a stronger tool.)
+                    // (But we're copying vanilla here, not fixing it. Oh well.)
+                    resetProgress();
+                    return;
+                }
+            }
+            if (hit.blockX != original_block.blockX || hit.blockY != original_block.blockY || hit.blockZ != original_block.blockZ) {
+                resetProgress();
+                return;
+            }
+        }
+        progress += hitBlock.getPlayerRelativeBlockHardness(player, shadowWorld, hit.blockX, hit.blockY, hit.blockZ);
+        player.swingItem();
+        byte packetType = progress == 0 ? HammerNetType.digStart : HammerNetType.digProgress;
+        if (progress >= 1) {
+            packetType = HammerNetType.digFinish;
+        }
+        sendDigPacket(packetType, hit);
+        if (progress >= 1) {
+            resetProgress();
+            resetClick();
+            Hammer.proxy.setShadowWorld();
+            try {
+                controller.onPlayerDestroyBlock(hit.blockX, hit.blockY, hit.blockZ, hit.sideHit);
             } finally {
                 Hammer.proxy.restoreRealWorld();
             }
         } else {
-            HammerClientProxy.shadowRenderGlobal.destroyBlockPartially(player.getEntityId(), hit.blockX, hit.blockY, hit.blockZ, int_progress);
+            HammerClientProxy.shadowRenderGlobal.destroyBlockPartially(player.getEntityId(), hit.blockX, hit.blockY, hit.blockZ, (int) (progress*10F) - 1);
+        }
+    }
+    
+    int left_click_delay = 5;
+    void resetClick() {
+        Minecraft mc = Minecraft.getMinecraft();
+        mc.gameSettings.keyBindAttack.pressTime = 1;
+        if (mc.playerController.isInCreativeMode()) {
+            left_click_delay = 5;
         }
     }
     
