ef9cccbac3c8d056602e0e3df0520ef179667f71
neptunepink <purpleposeidon@gmail.com>
2014-08-30 01:55:51 -0700

    Improved LMP Mask rendering

--- src/main/java/factorization/weird/NeptuneCape.java
+++ src/main/java/factorization/weird/NeptuneCape.java
@@ -1,16 +1,19 @@
 package factorization.weird;
 
-import org.lwjgl.opengl.GL11;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.entity.monster.EntityEnderman;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.client.event.TextureStitchEvent;
+
+import org.lwjgl.opengl.GL11;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import factorization.shared.Core;
 
@@ -19,22 +22,66 @@ public class NeptuneCape {
         Core.loadBus(this);
     }
     
-    ItemStack LMP = new ItemStack(Core.registry.logicMatrixProgrammer);
-    EntityEnderman dummy_entity = new EntityEnderman(null);
+    static boolean hideMask(EntityPlayer player) {
+        return player.getHideCape() || player.getCurrentArmor(3) != null;
+    }
     
-    class LmpMaskRenderer extends ModelRenderer {
+    static boolean should_render_mask = false;
+    private static int total_hash = 0;
+    private static final int[] mask_wearers = build_list("neptunepink");
+    private static int[] build_list(String... names) {
+        int[] ret = new int[names.length];
+        int hash_or = 0;
+        for (int i = 0; i < ret.length; i++) {
+            hash_or |= ret[i] = names[i].hashCode();
+        }
+        total_hash = ~hash_or;
+        return ret;
+    }
+
+    private static boolean isNameWhitelisted(String name) {
+        int name_hash = name.hashCode();
+        if ((name_hash & total_hash) != 0) return false;
+        for (int hash : mask_wearers) {
+            if (hash == name_hash) return true;
+        }
+        return false;
+    }
+    
+    @SubscribeEvent
+    public void renderLmp(RenderPlayerEvent.Pre event) {
+        EntityPlayer player = event.entityPlayer;
+        should_render_mask = !hideMask(player) && isNameWhitelisted(player.getCommandSenderName());
+        if (should_render_mask) {
+            LmpMaskRenderer.rendering_player = (AbstractClientPlayer) player;
+        }
+    }
+    
+
+    @SubscribeEvent
+    public void resourcePackChanged(TextureStitchEvent.Post event) {
+        RenderPlayer playerRenderer = (RenderPlayer) RenderManager.instance.entityRenderMap.get(EntityPlayer.class);
+        ModelRenderer head = playerRenderer.modelBipedMain.bipedHead;
+        if (head.childModels != null) {
+            for (ModelRenderer child : (Iterable<ModelRenderer>) head.childModels) {
+                if (child instanceof LmpMaskRenderer) return;
+            }
+        }
+        head.addChild(new LmpMaskRenderer(playerRenderer.modelBipedMain));
+    }
+    
+    static class LmpMaskRenderer extends ModelRenderer {
+        ItemStack LMP = new ItemStack(Core.registry.logicMatrixProgrammer);
+        EntityEnderman dummy_entity = new EntityEnderman(null);
+        static AbstractClientPlayer rendering_player;
         
-        AbstractClientPlayer player;
-        
-        public LmpMaskRenderer(AbstractClientPlayer player, ModelBase base) {
+        public LmpMaskRenderer(ModelBase base) {
             super(base);
-            this.player = player;
         }
         
         @Override
         public void render(float partial) {
-            if (player.getHideCape()) return;
-            if (player.getCurrentArmor(3) != null) return;
+            if (!should_render_mask) return;
             GL11.glPushMatrix();
             
             float s = 12F/16F; GL11.glScalef(s, s, s);
@@ -52,19 +99,9 @@ public class NeptuneCape {
             RenderManager.instance.itemRenderer.renderItem(dummy_entity, LMP, 0);
             GL11.glPopAttrib();
             GL11.glPopMatrix();
-            Minecraft.getMinecraft().renderEngine.bindTexture(player.getLocationSkin());
+            Minecraft.getMinecraft().renderEngine.bindTexture(rendering_player.getLocationSkin());
+            rendering_player = null;
         }
         
-    }
-    
-    @SubscribeEvent
-    public void renderLmp(RenderPlayerEvent.Pre event) {
-        EntityPlayer player = event.entityPlayer;
-        if (player.getHideCape()) return;
-        if (!"neptunepink".equals(player.getCommandSenderName())) return;
-        ModelRenderer head = event.renderer.modelBipedMain.bipedHead;
-        if (head.childModels == null) {
-            head.addChild(new LmpMaskRenderer((AbstractClientPlayer) player, event.renderer.modelBipedMain));
-        }
     }
 }
