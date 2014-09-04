0fc99fe2c9608d2a9f99f8b1c383a8e6b1a36b4a
neptunepink <purpleposeidon@gmail.com>
2014-09-03 18:11:53 -0700

    Skeleton code! \o/

--- src/main/java/factorization/colossi/ColossusController.java
+++ src/main/java/factorization/colossi/ColossusController.java
@@ -7,6 +7,8 @@ import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
+import factorization.api.Quaternion;
 import factorization.api.datahelpers.DataHelper;
 import factorization.api.datahelpers.DataInNBT;
 import factorization.api.datahelpers.DataOutNBT;
@@ -30,13 +32,22 @@ public class ColossusController extends Entity {
 
     
     static class LimbInfo {
-        static UUID fake_uuid = UUID.randomUUID();
+        static UUID fake_uuid = UUID.fromString("00000000-0000-0000-0000-000000000000");
         LimbType type = LimbType.UNKNOWN_LIMB_TYPE;
         BodySide side = BodySide.UNKNOWN_BODY_SIDE;
-        int length;
+        byte parity = 0; // If you have a centipede, you kind of need to swap left & right every other foot
+        int length; // How long the limb is
         UUID entityId = fake_uuid;
         IDeltaChunk ent = null;
-        Vec3 originalBodyOffset;
+        Vec3 originalBodyOffset; // Joint position, relative to the body's DSE.
+        
+        // The following transformations are applied in the order given here.
+        // They aren't relevant to LimbType.BODY.
+        // Angles are in radians.
+        double extension = 0; // Translation along the limb's local Y axis
+        double twist = 0; // Rotation on the local Y axis
+        double swing = 0; // Rotation on the local Z axis. Swinging arms/legs.
+        double sweep = 0; // Rotation on the global Y axis
         
         public LimbInfo() { }
         
@@ -52,6 +63,7 @@ public class ColossusController extends Entity {
         void putData(DataHelper data, int index) throws IOException {
             type = data.as(Share.PRIVATE, "limbType" + index).putEnum(type);
             side = data.as(Share.PRIVATE, "limbSide" + index).putEnum(side);
+            parity = data.as(Share.PRIVATE, "limbParity" + index).putByte(parity);
             length = data.as(Share.PRIVATE, "limbLength" + index).putInt(length);
             entityId = data.as(Share.PRIVATE, "entityId" + index).putUUID(entityId);
             if (data.isReader()) {
@@ -60,6 +72,10 @@ public class ColossusController extends Entity {
             originalBodyOffset.xCoord = data.as(Share.PRIVATE, "bodyX" + index).putDouble(originalBodyOffset.xCoord);
             originalBodyOffset.yCoord = data.as(Share.PRIVATE, "bodyY" + index).putDouble(originalBodyOffset.yCoord);
             originalBodyOffset.zCoord = data.as(Share.PRIVATE, "bodyZ" + index).putDouble(originalBodyOffset.zCoord);
+            extension = data.as(Share.PRIVATE, "extension" + index).putDouble(extension);
+            twist = data.as(Share.PRIVATE, "twist" + index).putDouble(twist);
+            swing = data.as(Share.PRIVATE, "swing" + index).putDouble(swing);
+            sweep = data.as(Share.PRIVATE, "sweep" + index).putDouble(sweep);
         }
         
         void setOffsetFromBody(IDeltaChunk body) {
@@ -99,17 +115,54 @@ public class ColossusController extends Entity {
             if (!setup) return;
         }
         LimbInfo body = null;
+        double TAU = Math.PI * 2;
         for (LimbInfo li : limbs) {
             if (li.type == LimbType.BODY) {
                 body = li;
             }
+            if (li.type == LimbType.ARM) { // NORELEASE LimbSide isn't being calculated right
+                li.extension = -0.0;
+                li.twist += (TAU * 1.0/8.0) / 20;
+                li.twist = 0;
+                li.swing = TAU * 0.40;
+                li.sweep = TAU / 8;
+                if (li.side == BodySide.LEFT) li.sweep *= -1;
+            }
+            if (li.type == LimbType.LEG) {
+                li.swing = TAU * 0.025;
+                if (li.side == BodySide.LEFT) li.swing *= -1;
+            }
         }
+        if (body == null) return; // !
+        Quaternion bodyRotation = body.ent.getRotation();
         for (LimbInfo limb : limbs) {
             if (limb == body) continue;
-            if (ae.tick(body, limb)) {
+            Vec3 joint = limb.originalBodyOffset.addVector(0, limb.extension, 0);
+            bodyRotation.applyRotation(joint);
+            Quaternion rot = new Quaternion(bodyRotation);
+            
+            Quaternion sweep = Quaternion.getRotationQuaternionRadians(limb.sweep, ForgeDirection.UP);
+            rot.incrMultiply(sweep);
+            
+            Quaternion swing = Quaternion.getRotationQuaternionRadians(limb.swing, ForgeDirection.SOUTH);
+            rot.incrMultiply(swing);
+            
+            Quaternion twist = Quaternion.getRotationQuaternionRadians(limb.twist, ForgeDirection.UP);
+            rot.incrMultiply(twist);
+            
+            
+            
+            
+            rot.incrNormalize();
+            limb.ent.setRotation(rot);
+            limb.ent.posX = body.ent.posX + joint.xCoord;
+            limb.ent.posY = body.ent.posY + joint.yCoord;
+            limb.ent.posZ = body.ent.posZ + joint.zCoord;
+            
+            
+            /*if (ae.tick(body, limb)) {
                 ae = new AnimationExecutor("legWalk");
-            }
-            break;
+            }*/
         }
     }
     
