dfb9887fb407c581e129a49a1a84a1631aa8326b
neptunepink <purpleposeidon@gmail.com>
2015-01-13 21:27:36 -0800

    Add unnecessarily detailed documentation to TechniqueKind

--- src/main/java/factorization/colossi/TechniqueKind.java
+++ src/main/java/factorization/colossi/TechniqueKind.java
@@ -1,5 +1,15 @@
 package factorization.colossi;
 
+/**
+ * Classifies how a Technique should be used
+ */
 public enum TechniqueKind {
-    OFFENSIVE, DEFENSIVE, IDLER, TRANSITION
+    /** Used to attack players. Technique.usabe() is called to see if it's okay to use. */
+    OFFENSIVE,
+    /** Only called after the colossus has been hurt. */
+    DEFENSIVE,
+    /** Used to kill time. */
+    IDLER,
+    /** An internal state. The technique-picker will never use one of these. */
+    TRANSITION;
 }
