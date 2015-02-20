2705f20aa5911324559e4836c88c1bb71030ae4b
neptunepink <purpleposeidon@gmail.com>
2015-02-20 02:47:42 -0800

    Don't let the wither break our bedrock

--- src/main/java/factorization/common/BlastedBedrock.java
+++ src/main/java/factorization/common/BlastedBedrock.java
@@ -3,6 +3,14 @@ package factorization.common;
 import factorization.shared.Core;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.Explosion;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class BlastedBedrock extends Block {
     protected BlastedBedrock() {
@@ -12,5 +20,29 @@ public class BlastedBedrock extends Block {
         setCreativeTab(Core.tabFactorization);
         setBlockName("factorization:blasted_bedrock");
         setBlockTextureName("factorization:blasted_bedrock");
+    }
+
+    @Override
+    public boolean canEntityDestroy(IBlockAccess world, int x, int y, int z, Entity entity) {
+        return false;
+    }
+
+    @Override
+    public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_) {
+        return null;
+    }
+
+    @Override
+    protected void dropBlockAsItem(World p_149642_1_, int p_149642_2_, int p_149642_3_, int p_149642_4_, ItemStack p_149642_5_) {
+    }
+
+    @Override
+    public boolean canDropFromExplosion(Explosion p_149659_1_) {
+        return false;
+    }
+
+    @Override
+    public void onBlockExploded(World world, int x, int y, int z, Explosion explosion) {
+        // Not explodable!
     }
 }
