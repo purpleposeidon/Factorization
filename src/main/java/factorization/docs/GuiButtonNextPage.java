b5c846ff7f6d7cf8e98ed3ad4578397f7084ec8f
neptunepink <purpleposeidon@gmail.com>
2015-03-11 19:28:12 -0700

    Add outlines to the prev/nextpage buttons

--- src/main/java/factorization/docs/GuiButtonNextPage.java
+++ src/main/java/factorization/docs/GuiButtonNextPage.java
@@ -13,7 +13,7 @@ public class GuiButtonNextPage extends GuiButton {
      * page).
      */
     private final boolean nextPage;
-    private static final ResourceLocation bookGuiTextures = new ResourceLocation("textures/gui/book.png");
+    private static final ResourceLocation bookGuiTextures = new ResourceLocation("factorization:textures/gui/book.png");
     
     public GuiButtonNextPage(int id, int posX, int posY, boolean next) {
         super(id, posX, posY, 23, 13, "");
@@ -23,24 +23,23 @@ public class GuiButtonNextPage extends GuiButton {
     /**
      * Draws this button to the screen.
      */
-    public void drawButton(Minecraft par1Minecraft, int par2, int par3) {
-        if (this.visible) {
-            boolean flag = par2 >= this.xPosition && par3 >= this.yPosition && par2 < this.xPosition + this.width
-                    && par3 < this.yPosition + this.height;
-            GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-            par1Minecraft.getTextureManager().bindTexture(bookGuiTextures);
-            int k = 0;
-            int l = 192;
+    public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+        if (!visible) return;
+        boolean flag = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width
+                && mouseY < this.yPosition + this.height;
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        mc.getTextureManager().bindTexture(bookGuiTextures);
+        int k = 0;
+        int l = 192;
 
-            if (flag) {
-                k += 23;
-            }
-
-            if (!this.nextPage) {
-                l += 13;
-            }
-
-            this.drawTexturedModalRect(this.xPosition, this.yPosition, k, l, 23, 13);
+        if (flag) {
+            k += 23;
         }
+
+        if (!this.nextPage) {
+            l += 13;
+        }
+
+        this.drawTexturedModalRect(this.xPosition, this.yPosition, k, l, 23, 13);
     }
 }
