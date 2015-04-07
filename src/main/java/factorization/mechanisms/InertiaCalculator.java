5ef0075f022a126cd9d4d8efa75e8301719996b4
neptunepink <purpleposeidon@gmail.com>
2015-04-07 04:21:36 -0700

    MassCalculator calculates the center of mass

--- src/main/java/factorization/mechanisms/InertiaCalculator.java
+++ src/main/java/factorization/mechanisms/InertiaCalculator.java
@@ -7,25 +7,22 @@ import factorization.util.SpaceUtil;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.Vec3;
 
-class InertiaCalculator implements ICoordFunction {
+class InertiaCalculator extends MassCalculator implements ICoordFunction {
     static double getInertia(IDeltaChunk idc, Vec3 axisOfRotation) {
         NBTTagCompound tag = idc.getEntityData();
-        if (checkAxis(tag, axisOfRotation) && tag.hasKey(inertiaKey)) return tag.getDouble(inertiaKey);
+        if (axisNotUpdated(tag, axisOfRotation) && tag.hasKey(inertiaKey)) return tag.getDouble(inertiaKey);
         final InertiaCalculator inertiaCalculator = new InertiaCalculator(idc, axisOfRotation);
-        double inertia = inertiaCalculator.calculate();
-        double mass = inertiaCalculator.massSum;
-        tag.setDouble(inertiaKey, inertia);
-        MassCalculator.saveMass(idc, mass);
-        return inertia;
+        inertiaCalculator.calculate();
+        return inertiaCalculator.inertiaSum;
     }
 
     static void dirty(IDeltaChunk idc) {
         final NBTTagCompound tag = idc.getEntityData();
         tag.removeTag(inertiaKey);
-        tag.removeTag(MassCalculator.massKey);
+        MassCalculator.dirty(idc);
     }
 
-    private static boolean checkAxis(NBTTagCompound tag, Vec3 axis) {
+    private static boolean axisNotUpdated(NBTTagCompound tag, Vec3 axis) {
         double lastX = tag.getDouble(axisKey + ".x");
         double lastY = tag.getDouble(axisKey + ".y");
         double lastZ = tag.getDouble(axisKey + ".z");
@@ -40,35 +37,31 @@ class InertiaCalculator implements ICoordFunction {
 
     private static final String inertiaKey = "IdcInertia";
     private static final String axisKey = "IdcInertiaAxis";
-    private final Coord min, max;
     private final Vec3 origin, axisOfRotation;
-    private double sum = 0;
-    private double massSum = 0;
+    private double inertiaSum = 0;
 
-    private InertiaCalculator(IDeltaChunk idc, Vec3 axisOfRotation) {
-        min = idc.getCorner();
-        max = idc.getFarCorner();
+    protected InertiaCalculator(IDeltaChunk idc, Vec3 axisOfRotation) {
+        super(idc);
         Vec3 minVec = SpaceUtil.newVec();
         min.setAsVector(minVec);
         origin = SpaceUtil.incrAdd(minVec, idc.getRotationalCenterOffset());
         this.axisOfRotation = axisOfRotation;
     }
 
-    private double calculate() {
-        Coord.iterateCube(min, max, this);
-        return sum;
-    }
-
     private Vec3 here = SpaceUtil.newVec();
+
     @Override
-    public void handle(Coord coordHere) {
+    public void handle(Coord coordHere, double blockMass) {
+        super.handle(coordHere, blockMass);
         coordHere.setAsVector(here);
         SpaceUtil.incrSubtract(here, origin);
         double d = SpaceUtil.lineDistance(axisOfRotation, here);
-        final double blockMass = MassHelper.getBlockMass(coordHere);
-        sum += blockMass * d * d;
-        massSum += blockMass;
+        inertiaSum += blockMass * d * d;
     }
 
-
+    @Override
+    protected void save(NBTTagCompound tag) {
+        super.save(tag);
+        tag.setDouble(inertiaKey, inertiaSum);
+    }
 }
--- src/main/java/factorization/mechanisms/MassCalculator.java
+++ src/main/java/factorization/mechanisms/MassCalculator.java
@@ -3,44 +3,87 @@ package factorization.mechanisms;
 import factorization.api.Coord;
 import factorization.api.ICoordFunction;
 import factorization.fzds.interfaces.IDeltaChunk;
+import factorization.util.NumUtil;
+import factorization.util.SpaceUtil;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.Vec3;
 
 class MassCalculator implements ICoordFunction {
     static double calculateMass(IDeltaChunk idc) {
         NBTTagCompound tag = idc.getEntityData();
         if (tag.hasKey(massKey)) return tag.getDouble(massKey);
-        double mass = new MassCalculator(idc).calculate();
-        saveMass(idc, mass);
-        return mass;
+        MassCalculator mc = new MassCalculator(idc);
+        mc.calculate();
+        return mc.massTotal;
     }
 
-    static void saveMass(IDeltaChunk idc, double mass) {
-        idc.getEntityData().setDouble(massKey, mass);
+    static Vec3 getCenterOfMass(IDeltaChunk idc) {
+        NBTTagCompound tag = idc.getEntityData();
+        if (tag.hasKey(comKey + ".x")) {
+            double x = tag.getDouble(comKey + ".x");
+            double y = tag.getDouble(comKey + ".y");
+            double z = tag.getDouble(comKey + ".z");
+            return Vec3.createVectorHelper(x, y, z);
+        }
+        MassCalculator mc = new MassCalculator(idc);
+        return mc.com;
+    }
+
+    static Coord getComCoord(IDeltaChunk idc) {
+        Vec3 com = SpaceUtil.floor(MassCalculator.getCenterOfMass(idc));
+        Coord min = idc.getCorner();
+        com.xCoord -= min.x;
+        com.yCoord -= min.y;
+        com.zCoord -= min.z;
+        return new Coord(idc.getCorner().w, com);
     }
 
     static void dirty(IDeltaChunk idc) {
         final NBTTagCompound tag = idc.getEntityData();
-        tag.removeTag(MassCalculator.massKey);
+        tag.removeTag(massKey);
+        tag.removeTag(comKey + ".x");
+        tag.removeTag(comKey + ".y");
+        tag.removeTag(comKey + ".z");
     }
 
-    static final String massKey = "IdcMass";
+    private static final String massKey = "IdcMass";
+    private static final String comKey = "IdcCOM";
 
-    private double sum = 0;
+    private double massTotal = 0;
+    private Vec3 com = SpaceUtil.newVec();
     private final IDeltaChunk idc;
+    protected final Coord min, max;
 
-    private MassCalculator(IDeltaChunk idc) {
+    protected MassCalculator(IDeltaChunk idc) {
         this.idc = idc;
+        min = idc.getCorner();
+        max = idc.getFarCorner();
     }
 
-    private double calculate() {
-        Coord min = idc.getCorner();
-        Coord max = idc.getFarCorner();
+    protected final void calculate() {
         Coord.iterateCube(min, max, this);
-        return sum;
+        save(idc.getEntityData());
+    }
+
+    protected void save(final NBTTagCompound tag) {
+        tag.setDouble(massKey, massTotal);
+        tag.setDouble(comKey + ".x", com.xCoord);
+        tag.setDouble(comKey + ".y", com.yCoord);
+        tag.setDouble(comKey + ".z", com.zCoord);
+    }
+
+    private Vec3 boxMid = SpaceUtil.newVec();
+    protected void handle(Coord here, double mass) {
+        SpaceUtil.setMiddle(here.getCollisionBoundingBoxFromPool(), boxMid);
+        double posRatio = mass / (mass + massTotal); // First iteration this will be 1, setting com = boxMid
+        NumUtil.interp(com, boxMid, (float) posRatio, com);
+        massTotal += mass;
     }
 
     @Override
-    public void handle(Coord here) {
-        sum += MassHelper.getBlockMass(here);
+    public final void handle(Coord here) {
+        final double hereMass = MassHelper.getBlockMass(here);
+        if (hereMass <= 0) return;
+        handle(here, hereMass);
     }
 }
