ebeb1cc2d457ec601ca7ed6038e6855dfe380c4b
neptunepink <purpleposeidon@gmail.com>
2015-04-08 00:17:06 -0700

    Bukkit breaks Hammer.

--- src/main/java/factorization/fzds/HammerEnabled.java
+++ src/main/java/factorization/fzds/HammerEnabled.java
@@ -1,6 +1,7 @@
 package factorization.fzds;
 
 import factorization.coremod.LoadingPlugin;
+import factorization.shared.Core;
 import net.minecraftforge.common.config.Configuration;
 
 import java.io.File;
@@ -9,11 +10,32 @@ public class HammerEnabled {
     public static final boolean ENABLED = isEnabled();
 
     private static boolean isEnabled() {
+        final String the_devil = "Bukkit/Craftbukkit/Cauldron/MCPC+";
+        boolean init = true;
+        try {
+            Class cl = Class.forName("org.bukkit.Bukkit");
+            if (cl != null) {
+                init = Boolean.parseBoolean(System.getProperty("fz.hammer.forceEnableWithBukkit", "false"));
+            }
+            if (!init) {
+                Core.logWarning(the_devil + " detected; disabling Hammer");
+            }
+        } catch (ClassNotFoundException e) {
+            // No bukkit
+        }
+
         final File configDirectory = new File(LoadingPlugin.getMcLocation(), "config");
-        if (!configDirectory.exists()) return true;
+        if (!configDirectory.exists()) return init;
         File cfgName = new File(configDirectory, "hammerChannels.cfg");
         Configuration config = new Configuration(cfgName);
-        boolean ret = config.getBoolean("enabled", "hammer", true, "Set to false to disable FZDS. Setting to false will disable colossi, hinges, twisted blocks, etc.");
+        String comment = "Set to false to disable FZDS. Setting to false will disable colossi, hinges, twisted blocks, etc.";
+        if (!init) {
+            comment = "Force-disabled by " + the_devil + "; setting to true will do nothing.";
+        }
+        boolean ret = config.getBoolean("enabled", "hammer", init, comment);
+        if (!init) {
+            ret = false;
+        }
         if (config.hasChanged()) {
             config.save();
         }
