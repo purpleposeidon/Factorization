b21c3e3a4b1eb0b35973197f391ad60a61f1ed96
neptunepink <purpleposeidon@gmail.com>
2015-05-18 18:00:19 -0700

    Update patreon barrel recipes

--- src/main/java/factorization/shared/PatreonRewards.java
+++ src/main/java/factorization/shared/PatreonRewards.java
@@ -5,10 +5,14 @@ import factorization.weird.TileEntityDayBarrel;
 import net.minecraft.item.ItemStack;
 
 public class PatreonRewards {
-    private static final byte CHEAP = 0, FINE = 1, EXPENSIVE = 2;
+    private static final byte CHEAP = 0, NORMAL = 1, EXPENSIVE = 2;
 
     static void init() {
         // NORELEASE: Patron update
+        customBarrel("neptunepink", "Forever", NORMAL, "Thaumcraft:blockMagicalLog#0", "Thaumcraft:blockCosmeticSlabWood#0");
+        customBarrel("neptunepink", "Forever", NORMAL, "Thaumcraft:blockMagicalLog#1", "Thaumcraft:blockCosmeticSlabWood#1");
+        customBarrel("SoundLogic", "May 2015", NORMAL, "Botania:livingwood#0", "Botania:livingwood#1");
+        customBarrel("SoundLogic", "May 2015", NORMAL, "Botania:dreamwood#0", "Botania:dreamwood#1"); // *two* barrel recipes!? Oh well; they're a pair.
         customBarrel("mallrat208", "Feb 2015", EXPENSIVE, "factorization:ResourceBlock#3", "factorization:ResourceBlock#2");
     }
 
