70b9ca469e55678a6553b4efcf4ff31a48e172d0
neptunepink <purpleposeidon@gmail.com>
2015-06-22 15:56:37 -0700

    Make the rail step skinnier

--- src/main/java/factorization/coremodhooks/MixinRailStairs.java
+++ src/main/java/factorization/coremodhooks/MixinRailStairs.java
@@ -18,17 +18,18 @@ public class MixinRailStairs extends BlockRailBase {
         if (ent instanceof EntityPlayer) {
             final double h = 0.5;
             final double w = 0.5;
+            final double s = 6.0 / 16.0;
             AxisAlignedBB box = null;
             BlockRailBase me = this;
             int i = me.getBasicRailMetadata(world, null, x, y, z);
             if (i == 0x2) { // Ascend East
-                box = AxisAlignedBB.getBoundingBox(x + w, y, z, x + 1, y + h, z + 1);
+                box = AxisAlignedBB.getBoundingBox(x + w, y, z + s, x + 1, y + h, z + 1 - s);
             } else if (i == 0x3) { // Ascend West
-                box = AxisAlignedBB.getBoundingBox(x, y, z, x + w, y + h, z + 1);
+                box = AxisAlignedBB.getBoundingBox(x, y, z + s, x + w, y + h, z + 1 - s);
             } else if (i == 0x4) { // Ascend North
-                box = AxisAlignedBB.getBoundingBox(x, y, z, x + 1, y + h, z + w);
+                box = AxisAlignedBB.getBoundingBox(x + s, y, z, x + 1 - s, y + h, z + w);
             } else if (i == 0x5) { // Ascend South
-                box = AxisAlignedBB.getBoundingBox(x, y, z + w, x + 1, y + h, z + 1);
+                box = AxisAlignedBB.getBoundingBox(x + s, y, z + w, x + 1 - s, y + h, z + 1);
             }
             if (box != null && queryBox.intersectsWith(box)) {
                 boxList.add(box);
