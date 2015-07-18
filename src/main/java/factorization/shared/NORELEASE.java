5a22c335d13b2fb646e75a45bbc65ecb344528f4
neptunepink <purpleposeidon@gmail.com>
2015-07-18 04:33:43 -0700

    Add NORELEASE.catcher

--- src/main/java/factorization/shared/NORELEASE.java
+++ src/main/java/factorization/shared/NORELEASE.java
@@ -13,24 +13,29 @@ import com.google.common.base.Joiner;
  * This class will be present at dev time, but must be excluded at compile time for non-test builds.
  * 
  */
+@SuppressWarnings("unused")
 public class NORELEASE {
     /**
      * Use with || NORELEASE.on
+     * NORELEASE.just may be better.
      */
     public static boolean on = true;
     
     /**
      * Use with && NORELEASE.off
+     * NORELEASE.just may be better.
      */
     public static boolean off = false;
     
     /**
-     * Use to adjust numeric values, such as int constant = 0xBEEF + 23 * NORELEASE.one
+     * Use to adjust numeric values, such as int constant = 0xBEEF + 23 * NORELEASE.one.
+     * NORELEASE.just may be better.
      */
     public static int one = 1;
     
     /**
      * Use to adjust numeric values, such as int minVal = 10 * NORELEASE.zero
+     * NORELEASE.just may be better.
      */
     public static int zero = 0;
     
@@ -77,4 +82,17 @@ public class NORELEASE {
     public static String str1 = "", str2 = "", str3 = "", str4 = "";
     public static long l1 = 0, l2 = 0, l3 = 0, l4 = 0;
     public static Object obj1 = null, obj2 = null, obj3 = null, obj4 = null;
+
+    /**
+     * Use this method to hotswap a try/catch clause.
+     */
+    public static Object catcher(Object input) {
+        try {
+            NORELEASE.just(input); // Keep this call here to ensure that this doesn't get optimized out
+        } catch (Throwable t) {
+            t.printStackTrace();;
+            NORELEASE.breakpoint();
+        }
+        return null;
+    }
 }
