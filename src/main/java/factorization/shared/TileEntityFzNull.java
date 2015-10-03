5cecb12fe56e02d7b633d2a8415b403da63ad0c3
neptunepink <purpleposeidon@gmail.com>
2015-10-03 15:40:08 -0700

    Log when hack-loading an fz.null

--- src/main/java/factorization/shared/TileEntityFzNull.java
+++ src/main/java/factorization/shared/TileEntityFzNull.java
@@ -11,11 +11,20 @@ public class TileEntityFzNull extends TileEntity {
         // Hack for pistronics: load the TE specified by that tag at this position.
         if (worldObj == null) return;
         if (mapName.equals(tag.getString("id"))) return; // Avoid recursion death
+        {
+            int origX = tag.getInteger("x");
+            int origY = tag.getInteger("y");
+            int origZ = tag.getInteger("z");
+            Core.logSevere("fz.null TileEntity reading a " + tag.getString("id") + " TileEntity at "
+                    + xCoord + "," + yCoord + "," + zCoord + "; saved position's at "
+                    + origX + "," + origY + "," + origZ);
+        }
         tag.setInteger("x", this.xCoord);
         tag.setInteger("y", this.yCoord);
         tag.setInteger("z", this.zCoord);
         TileEntity replacement = TileEntity.createAndLoadEntity(tag);
         if (replacement == null) return; // Nothing can be done here.
+        invalidate();
         replacement.validate();
         worldObj.setTileEntity(xCoord, yCoord, zCoord, replacement);
     }
