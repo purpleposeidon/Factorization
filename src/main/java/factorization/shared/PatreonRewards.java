6b18ad684af75d7985b0125939f06d79b25434b1
neptunepink <purpleposeidon@gmail.com>
2015-12-10 14:36:49 -0800

    Any of my patreons can Masquerade!

--- src/main/java/factorization/shared/PatreonRewards.java
+++ src/main/java/factorization/shared/PatreonRewards.java
@@ -4,6 +4,9 @@ import factorization.util.ItemUtil;
 import factorization.weird.TileEntityDayBarrel;
 import net.minecraft.item.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PatreonRewards {
     private static final byte CHEAP = 0, NORMAL = 1, EXPENSIVE = 2;
 
@@ -14,9 +17,11 @@ public class PatreonRewards {
         customBarrel("SoundLogic", "May 2015", NORMAL, "Botania:livingwood#0", "Botania:livingwood#1");
         customBarrel("SoundLogic", "May 2015", NORMAL, "Botania:dreamwood#0", "Botania:dreamwood#1"); // *two* barrel recipes!? Oh well; they're a pair.
         customBarrel("mallrat208", "Feb 2015", EXPENSIVE, "factorization:ResourceBlock#3", "factorization:ResourceBlock#2");
+        masquerader("asiekierka", "Dec 2015");
     }
 
     private static void customBarrel(String patron, String date, byte cost, String logName, String slabName) {
+        masquerader(patron, date);
         ItemStack log = ItemUtil.parseBlock(logName);
         ItemStack slab = ItemUtil.parseBlock(slabName);
         if (log == null || slab == null) return;
@@ -24,14 +29,14 @@ public class PatreonRewards {
     }
 
     private static void masquerader(String patron, String date) {
-
+        masqueraders.add(patron);
     }
 
-    /**
-     * Don't use this from the renderer! Have the LMP set its damage value to something when put on!
-     */
-    public static boolean isMasquerader(String username) {
-        // Or UUID or something?
-        return false;
+    private static ArrayList<String> masqueraders = new ArrayList<String>();
+
+    public static List<String> getMasqueraders() {
+        ArrayList<String> ret = new ArrayList<String>();
+        ret.addAll(masqueraders);
+        return ret;
     }
 }
--- src/main/java/factorization/weird/NeptuneCape.java
+++ src/main/java/factorization/weird/NeptuneCape.java
@@ -1,19 +1,22 @@
 package factorization.weird;
 
 import factorization.shared.Core;
+import factorization.shared.FzModel;
+import factorization.shared.PatreonRewards;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
-import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderPlayer;
-import net.minecraft.entity.monster.EntityEnderman;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.opengl.GL11;
+
+import java.util.List;
 
 public class NeptuneCape {
     {
@@ -27,12 +30,12 @@ public class NeptuneCape {
     
     static boolean should_render_mask = false;
     private static int total_hash = 0;
-    private static final int[] mask_wearers = build_list("neptunepink");
-    private static int[] build_list(String... names) {
-        int[] ret = new int[names.length];
+    private static final int[] mask_wearers = build_list(PatreonRewards.getMasqueraders());
+    private static int[] build_list(List<String> names) {
+        int[] ret = new int[names.size()];
         int hash_or = 0;
         for (int i = 0; i < ret.length; i++) {
-            hash_or |= ret[i] = names[i].hashCode();
+            hash_or |= ret[i] = names.get(i).hashCode();
         }
         total_hash = ~hash_or;
         return ret;
@@ -49,31 +52,49 @@ public class NeptuneCape {
     
     @SubscribeEvent
     public void renderLmp(RenderPlayerEvent.Pre event) {
+        if (invalid) return;
         EntityPlayer player = event.entityPlayer;
+        if (dirty) {
+            dirty = false;
+            injectModel(player);
+        }
         should_render_mask = !hideMask(player) && isNameWhitelisted(player.getCommandSenderName());
         if (should_render_mask) {
             LmpMaskRenderer.rendering_player = (AbstractClientPlayer) player;
         }
     }
-    
 
-    @SubscribeEvent
-    public void resourcePackChanged(TextureStitchEvent.Post event) {
-        RenderPlayer playerRenderer = (RenderPlayer) RenderManager.instance.entityRenderMap.get(EntityPlayer.class);
-        ModelRenderer head = playerRenderer.modelBipedMain.bipedHead;
+    boolean invalid = false;
+    void injectModel(EntityPlayer player) {
+        Minecraft mc = Minecraft.getMinecraft();
+        Render<EntityPlayer> pr = mc.getRenderManager().getEntityRenderObject(player);
+        if (!((Render) pr instanceof RenderPlayer)) {
+            if (!invalid) {
+                Core.logWarning("Unable to patch Player model!");
+            }
+            invalid = true;
+            return;
+        }
+        RenderPlayer playerRenderer = (RenderPlayer) (Render) pr;
+        ModelRenderer head = playerRenderer.getMainModel().bipedHead;
         if (head.childModels != null) {
-            for (ModelRenderer child : (Iterable<ModelRenderer>) head.childModels) {
+            for (ModelRenderer child : head.childModels) {
                 if (child instanceof LmpMaskRenderer) return;
             }
         }
-        head.addChild(new LmpMaskRenderer(playerRenderer.modelBipedMain));
+        head.addChild(new LmpMaskRenderer(playerRenderer.getMainModel()));
+    }
+    
+    boolean dirty = true;
+    @SubscribeEvent
+    public void resourcePackChanged(TextureStitchEvent.Post event) {
+        dirty = true;
     }
     
     static class LmpMaskRenderer extends ModelRenderer {
-        ItemStack LMP = new ItemStack(Core.registry.logicMatrixProgrammer);
-        EntityEnderman dummy_entity = new EntityEnderman(null);
+        FzModel LMP = new FzModel("lmpMask");
         static AbstractClientPlayer rendering_player;
-        
+
         public LmpMaskRenderer(ModelBase base) {
             super(base);
             base.boxList.remove(this); // Prevents arrows from rendering inside us; else rendering will crash
@@ -96,9 +117,7 @@ public class NeptuneCape {
             GL11.glRotatef(-50.0F, 0.0F, 1.0F, 0.0F);
             
             
-            GL11.glEnable(GL11.GL_CULL_FACE); // Pretty necessary for 3D-item rendering.
-            RenderManager.instance.itemRenderer.renderItem(dummy_entity, LMP, 0);
-            GL11.glDisable(GL11.GL_CULL_FACE); // It's the default for entity rendering.
+            LMP.draw();
             GL11.glPopMatrix();
             Minecraft.getMinecraft().renderEngine.bindTexture(rendering_player.getLocationSkin());
             rendering_player = null;
