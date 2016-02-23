58599349292ccecf858799915fe640545897c8c8
neptunepink <purpleposeidon@gmail.com>
2016-02-22 20:03:08 -0800

    Trying to figure this out...

--- src/main/java/factorization/beauty/TileEntityBiblioGenRenderer.java
+++ src/main/java/factorization/beauty/TileEntityBiblioGenRenderer.java
@@ -16,16 +16,9 @@ public class TileEntityBiblioGenRenderer extends TileEntitySpecialRenderer<TileE
         float angle = (float) Math.toDegrees(NumUtil.interp(gen.prev_angle, gen.angle, partialTicks));
         GL11.glRotatef(-angle, 0, 1, 0);
         GL11.glTranslatef(0, 4.125F / 16F, 0);
-        drawBook();
-        GL11.glPopMatrix();
-    }
-
-    void drawBook() {
         double s = 5.0 / 4.0;
         GL11.glScaled(s, s, s);
-        GL11.glPushAttrib(GL11.GL_TRANSFORM_BIT);
-        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
         bookModel.draw();
-        GL11.glPopAttrib();
+        GL11.glPopMatrix();
     }
 }
