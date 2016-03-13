fe16061b0804c4622d5e97a4b274fc4771426ed6
neptunepink <purpleposeidon@gmail.com>
2016-03-13 14:16:26 -0700

    This is no longer necessary \o/

--- src/main/java/factorization/misc/MiscClientProxy.java
+++ src/main/java/factorization/misc/MiscClientProxy.java
@@ -12,7 +12,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiOptions;
-import net.minecraft.client.gui.GuiSelectWorld;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.resources.AbstractResourcePack;
 import net.minecraft.client.resources.IResource;
@@ -21,11 +20,8 @@ import net.minecraft.client.settings.GameSettings;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.EnumDifficulty;
 import net.minecraftforge.client.ClientCommandHandler;
-import net.minecraftforge.client.event.GuiScreenEvent.ActionPerformedEvent;
 import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
@@ -72,42 +68,7 @@ public class MiscClientProxy extends MiscProxy {
         newTps = Math.min(1.5F, Math.max(FzConfig.lowest_dilation, newTps));
         mc.timer.timerSpeed = newTps;
     }
-    
-    private GuiButton difficulty_button = null;
-    
-    @SubscribeEvent
-    public void addDifficultyInfo(InitGuiEvent.Post event) {
-        if (!(event.gui instanceof GuiSelectWorld)) {
-            difficulty_button = null;
-            return;
-        }
-        if (difficulty_button != null) return;
-        if (getDifficulty() != EnumDifficulty.PEACEFUL) return;
 
-        GuiButton first = (GuiButton) event.buttonList.get(0);
-        first.displayString = "    " + EnumChatFormatting.BOLD + EnumChatFormatting.RED + first.displayString;
-        event.buttonList.add(difficulty_button = new GuiButton(-237, 0, 0, 150, 18, ""));
-        updateDifficultyString();
-    }
-    
-    @SubscribeEvent
-    public void changeDifficulty(ActionPerformedEvent.Pre event) {
-        if (event.button != difficulty_button || difficulty_button == null) return;
-        GameSettings gs = Minecraft.getMinecraft().gameSettings;
-        gs.difficulty = FzUtil.shiftEnum(gs.difficulty, EnumDifficulty.values(), 1);
-        updateDifficultyString();
-    }
-    
-    void updateDifficultyString() {
-        EnumDifficulty ed = getDifficulty();
-        String color = (ed == EnumDifficulty.PEACEFUL) ? ("" + EnumChatFormatting.RED) : "";
-        difficulty_button.displayString = color + "Difficulty: " + ed;
-    }
-    
-    EnumDifficulty getDifficulty() {
-        return Minecraft.getMinecraft().gameSettings.difficulty;
-    }
-    
     @SubscribeEvent
     public void patchupTheStupidSecretButton(InitGuiEvent.Post event) {
         if (!(event.gui instanceof GuiOptions)) return;
