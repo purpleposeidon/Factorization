981622e5a225bf3c41f95ef7a51c220567fd1b2f
neptunepink <purpleposeidon@gmail.com>
2016-03-15 19:29:57 -0700

    Actually Seriously Get glPolygonoffset Working For Real This Time Guys
    (Player's right as usual. But reloading classes is faster than reloading
    resource packs.)

--- src/main/java/factorization/flat/render/EntityHackRender.java
+++ src/main/java/factorization/flat/render/EntityHackRender.java
@@ -48,10 +48,34 @@ public final class EntityHackRender extends Render<EntityHack> {
         cri.update(entity, entity.chunk, layer, entity.slabY);
         {
             GlStateManager.disableLighting();
-            GL11.glEnable(GL11.GL_POLYGON_OFFSET_FILL);
-            GlStateManager.doPolygonOffset(-3, -3);
+            // Big Table o' glPolygonOffset Tests
+            //(-3, -3) // What vanilla uses. Has z-fighting when looking straight down.
+            //(3, 3) // invisible, except for z-fighting
+            //(0, 1) // *very* z-fighty
+            //(0, -1) // *very* z-fighty
+            //(1, 0) // mostly invisible
+            //(-1, 0) // visible; still z-fighting
+            //(-10, 0) // visible; still z-fighting
+            //(-100, 0) // visible; still z-fighting
+            //(0, 100) // invisible, but no z-fighting
+            //(0, -100) // z-fighting at extreme angles (looking past edge + shifting)
+            //(-100, -100) // Seems good! But very extreme.
+            //(-10, -10) // z-fighting when looking down
+            //(-50, -50) // Works. Can it be lowered?
+            //(-12, -12) // Z-fighting when looking down
+            //(-25, -25) // Too extreme. It peaks through blocks!
+            //(-25, 0) // Still peeks, also z-fights
+            //(-3, 0) // No peeking, z-fights
+            //(-3, -25) // Pretty good, but there's minor peeking.
+            //(-3, -8) // z-fights; still peeks
+            //(-3, -4) // z-fights; still peeks
+            //(-1, -25) // Seems perfect!
+            //(0, -25) // z-fighting
+            GlStateManager.doPolygonOffset(-1, -12);
+            GlStateManager.enablePolygonOffset();
             cri.draw(entity);
-            GL11.glDisable(GL11.GL_POLYGON_OFFSET_FILL);
+            GlStateManager.disablePolygonOffset();
+            GlStateManager.doPolygonOffset(0, 0);
             GlStateManager.enableLighting();
         }
 
