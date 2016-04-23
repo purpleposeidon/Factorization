aa65c6a87d42edef2492fade219d5cb35f960e36
neptunepink <purpleposeidon@gmail.com>
2016-04-23 16:01:12 -0700

    Hinge model

--- src/main/java/factorization/mechanics/TileEntityHingeRenderer.java
+++ src/main/java/factorization/mechanics/TileEntityHingeRenderer.java
@@ -1,14 +1,17 @@
 package factorization.mechanics;
 
+import factorization.shared.FzModel;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import org.lwjgl.opengl.GL11;
 
 public class TileEntityHingeRenderer extends TileEntitySpecialRenderer<TileEntityHinge> {
+    FzModel top = new FzModel("hingeTop");
+
     @Override
     public void renderTileEntityAt(TileEntityHinge te, double cameraX, double cameraY, double cameraZ, float partial, int damage) {
         GL11.glPushMatrix();
         GL11.glTranslated(cameraX, cameraY, cameraZ);
-        //te.renderTesr(partial);
+        top.draw();
         GL11.glPopMatrix();
     }
 }
